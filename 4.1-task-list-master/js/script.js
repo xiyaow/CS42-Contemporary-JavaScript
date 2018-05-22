7c53891043f1e0e491110b9e63c6bfc4f6d991a6
xiyao <xiyaow@andrew.cmu.edu>
2018-05-23 01:17:38 +0800

    complete

--- 4.1-task-list-master/js/script.js
+++ 4.1-task-list-master/js/script.js
@@ -1,1 +1,40 @@
-// TODO: task list code
+const input = document.querySelector('#task')
+
+
+input.addEventListener('keydown', event => {
+	if(event.code === 'Enter'){
+		event.preventDefault()
+		addList()
+		input.value = ''
+		input.blur()
+	}
+})
+
+function addList(){
+	const list = document.createElement("li")
+	list.innerHTML = input.value
+	const checkSpan = document.createElement("span")
+	const deleteSpan = document.createElement("span")
+	checkSpan.classList.add("check")
+	deleteSpan.classList.add("delete")
+	list.appendChild(deleteSpan)
+	list.appendChild(checkSpan)
+	document.querySelector('#task-list').appendChild(list)
+	
+	addListener(list)
+}
+
+
+function addListener(list){
+	const selectCheck = list.querySelector('.check')
+	selectCheck.addEventListener('click', () => {
+		list.classList.toggle('checked')
+	})
+	
+	const selectDelete = list.querySelector('.delete')
+	selectDelete.addEventListener('click', () => {
+		list.parentNode.removeChild(list)
+	})
+
+}
+	
